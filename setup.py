70d8ad148510a9aeb4c8af5562379ab498644535
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2019-07-02 17:02:36 +0600

    pytest-black-isort-fix-works-successfully

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+
+setuptools.setup(
+    name="shamsul-package-name",
+    version="0.0.1",
+    author="Shamsul arefin",
+    author_email="author@example.com",
+    description="A small example package",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
